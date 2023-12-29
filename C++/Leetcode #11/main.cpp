252f86c7b5a58f273e8c7dc234b431bd6591d799
rrumana <ryan.rumana@gmail.com>
2023-12-29 02:57:44 -0800

    Added solution for Leetcode #11

--- C++/Leetcode #11/main.cpp
+++ C++/Leetcode #11/main.cpp
@@ -1,56 +1,56 @@
 #include <iostream>     // std::cout
-#include <algorithm>    // std::sort
+#include <algorithm>    // std::max / std::min
 #include <vector>       // std::vector
 
 class Solution {
 public:
-    int maxOperations(std::vector<int>& nums, int k) {
-        int i = 0, j = nums.size() - 1, cnt = 0;
-        std::sort(nums.begin(), nums.end());
-        while (i < j) {
-            if (nums[i] + nums[j] == k) {
-                i++;
-                j--;
-                cnt++;
-            } else if (nums[i] + nums[j] < k) {
-                i++;
-            } else {
-                j--;
-            }
+    int maxArea(std::vector<int>& height) {
+        int maximum = 0, i = 0,  j = height.size()-1;
+        while(i < j){
+            maximum = std::max(maximum, (j-i)*std::min(height[i], height[j]));
+            if(height[i] > height[j]) j--;
+            else i++;
         }
-        return cnt;
+        return maximum;
     }
 };
 
 void test1() {
-    std::vector<int> nums = {1,2,3,4};
-    int k = 5;
-    int expected = 2;
+    std::vector<int> nums = {1,8,6,2,5,4,8,3,7};
+    int expected = 49;
 
     Solution solution;
-	int output = solution.maxOperations(nums, k);
+	int output = solution.maxArea(nums);
 
     if(output == expected) std::cout<<"Passed test 1"<<std::endl;
 	else std::cout<<"Failed test 1"<<std::endl;
 }
 
 void test2() {
-    std::vector<int> nums = {3,1,3,4,3};
-    int k = 6;
+    std::vector<int> nums = {1,1};
     int expected = 1;
 
     Solution solution;
-	int output = solution.maxOperations(nums, k);
+	int output = solution.maxArea(nums);
 
     if(output == expected) std::cout<<"Passed test 2"<<std::endl;
 	else std::cout<<"Failed test 2"<<std::endl;
 }
 
-void test3() {}
+void test3() {
+    std::vector<int> nums = {2,3,4,5,18,17,6};
+    int expected = 16;
+
+    Solution solution;
+	int output = solution.maxArea(nums);
+
+    if(output == expected) std::cout<<"Passed test 3"<<std::endl;
+	else std::cout<<"Failed test 3"<<std::endl;
+}
 
 int main() {
     test1();
     test2();
-    //test3();
+    test3();
 	return 0;
 }

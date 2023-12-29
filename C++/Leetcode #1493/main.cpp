fd8cb08b6252f42f7b66b80ac4323e248acee03a
rrumana <ryan.rumana@gmail.com>
2023-12-29 13:29:49 -0800

    Restored Leedtocde #1493 implementation

--- C++/Leetcode #1493/main.cpp
+++ C++/Leetcode #1493/main.cpp
@@ -1,80 +1,86 @@
 #include <iostream>     // std::cout
-#include <algorithm>    // std::max
 #include <vector>       // std::vector
 
 class Solution {
 public:
-    int longestOnes(std::vector<int>& nums, int k) {
-        int start=0,zc=0;
-        int ans=0;
-        for(int end=0; end<nums.size(); end++){
-            if(nums[end]==0){
-                zc++;
+    int longestSubarray(std::vector<int>& nums) {
+        int count1=0;
+        int count2=0;
+        int sum=0;
+        bool flag = true;
+
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==0){
+                flag = false;
+                break;
             }
-            while(zc>k){
-                if(nums[start]==0){
-                    zc--;
-                }
-                start++;
-            }
-            ans=std::max(end-start+1,ans);
         }
-        return ans;
+
+        if(flag) return nums.size()-1;
+
+        for(int i=0; i<nums.size(); i++){
+            if(i==0 && nums[i]==0 || i==nums.size()-1 && nums[i]==0){
+                continue;
+            }
+            if(count1+count2>sum){
+                sum=count1+count2;
+            }
+            if(nums[i]==1){
+                count2++;
+            }
+            else if (nums[i-1]==1 && nums[i+1]==1){
+                count1=count2;
+                count2=0;
+            }
+            else {
+                count1=0;
+                count2=0;
+            }
+            
+        }
+        if(count1+count2>sum){
+                sum=count1+count2;
+            }
+        return sum;
     }
 };
 
 void test1() {
-    std::vector<int> nums = {1,1,1,0,0,0,1,1,1,1,0};
-    int k = 2;
-    int expected = 6;
+    std::vector<int> nums = {1,1,0,1};
+    int expected = 3;
 
     Solution solution;
-	int output = solution.longestOnes(nums, k);
+	int output = solution.longestSubarray(nums);
 
     if(output == expected) std::cout<<"Passed test 1"<<std::endl;
 	else std::cout<<"Failed test 1"<<std::endl;
 }
 
 void test2() {
-    std::vector<int> nums = {0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1};
-    int k = 3;
-    int expected = 10;
+    std::vector<int> nums = {0,1,1,1,0,1,1,0,1};
+    int expected = 5;
 
     Solution solution;
-	int output = solution.longestOnes(nums, k);
+	int output = solution.longestSubarray(nums);
 
     if(output == expected) std::cout<<"Passed test 2"<<std::endl;
 	else std::cout<<"Failed test 2"<<std::endl;
 }
 
 void test3() {
-    std::vector<int> nums = {0,0,1,1,1,0,0};
-    int k = 0;
-    int expected = 3;
+    std::vector<int> nums = {1,1,1};
+    int expected = 2;
 
     Solution solution;
-	int output = solution.longestOnes(nums, k);
+	int output = solution.longestSubarray(nums);
 
     if(output == expected) std::cout<<"Passed test 3"<<std::endl;
 	else std::cout<<"Failed test 3"<<std::endl;
-}
-
-void test4() {
-    std::vector<int> nums = {0,0,0,1};
-    int k = 4;
-    int expected = 4;
-
-    Solution solution;
-	int output = solution.longestOnes(nums, k);
-
-    if(output == expected) std::cout<<"Passed test 4"<<std::endl;
-	else std::cout<<"Failed test 4"<<std::endl;
 }
 
 int main() {
     test1();
     test2();
     test3();
-    test4();
 	return 0;
 }

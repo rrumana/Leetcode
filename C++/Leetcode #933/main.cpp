8d66a789f3f102d2521e03719844bb447f4513a6
rrumana <ryan.rumana@gmail.com>
2024-01-05 05:45:34 -0800

    Added Leetcode 933 Implementation

--- C++/Leetcode #933/main.cpp
+++ C++/Leetcode #933/main.cpp
@@ -3,11 +3,18 @@
 #include <queue>
 
 class RecentCounter {
+private:
+    std::queue<int> requests;
 public:
-    RecentCounter() {}
+    RecentCounter() {
+    }
     
     int ping(int t) {
-
+        requests.push(t);
+        while(requests.front() < t-3000){
+            requests.pop();
+        }
+        return requests.size();
     }
 };
 

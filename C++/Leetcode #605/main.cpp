24d2601ff01bc3694b3546501fd8adf06559ce33
rrumana <ryan.rumana@gmail.com>
2024-03-26 20:43:05 -0700

    Finished Leetcode #605

--- C++/Leetcode #605/main.cpp
+++ C++/Leetcode #605/main.cpp
@@ -1,109 +1,47 @@
 #include <iostream>         // std::cout
 #include <vector>           // std::vector
 
-/**
- * Definition for singly-linked list.
- */
-struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode() : val(0), left(nullptr), right(nullptr) {}
-  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
 class Solution {
   public:
-    TreeNode* searchBST(TreeNode* root, int val) {
-      if(root == NULL || root->val == val) return root;
-      else if(root->val > val) return searchBST(root->left, val);
-      else return searchBST(root->right, val);
+    bool canPlaceFlowers(std::vector<int>& flowerbed, int n) {
+      int count = 0;
+      for (int i = 0; i < flowerbed.size(); i++) {
+        if(flowerbed[i] == 0) {
+          int prev = i ? flowerbed[i - 1] : 0;
+          int next = (i == flowerbed.size() - 1) ? 0 : flowerbed[i + 1];
+          if(prev == 0 && next == 0) {
+            flowerbed[i] = 1;
+            count++;
+          }
+        }
+      }
+      return count >= n;
     }
 };
 
-TreeNode* newNode(int value) {
-  TreeNode* node = (TreeNode*)malloc(sizeof(TreeNode));
-  node->val = value;
-  node->left = NULL;
-  node->right = NULL;
-  return node;
-}
-
-TreeNode* newNode(int value, TreeNode* left, TreeNode* right) {
-  TreeNode* node = (TreeNode*)malloc(sizeof(TreeNode));
-  node->val = value;
-  node->left = left;
-  node->right = right;
-  return node;
-}
-
-
-TreeNode* constructTree(std::vector<int> vec, int index, int size) {
-  TreeNode *root = nullptr;
-
-  if (index < size) {
-    root = newNode(vec[index]);
-
-    root->left = constructTree(vec, 2 * index + 1, size);
-    root->right = constructTree(vec, 2 * index + 2, size);
-  }
-
-  return root;
-}
-
-void displayAll(TreeNode* root) {
-  if(root) {
-    std::cout<<root->val<<" ";
-    displayAll(root->left);
-    displayAll(root->right);
-  }
-}
-
-bool areIdentical(struct TreeNode *a, struct TreeNode *b) {
-  if (a == NULL && b == NULL) return true;
-  if (a == NULL || b == NULL) return false;
-
-  return (a->val == b->val && areIdentical(a->left, b->left) && areIdentical(a->right, b->right));
-}
-
 void test1() {
-  std::vector<int> InputTree = {4,2,7,1,3};
-  std::vector<int> OutputTree = {2,1,3};
+  std::vector<int> flowerbed = {1,0,0,0,1};
+  int n = 1;
 
-  int root = 2;
-
-  TreeNode *input = constructTree(InputTree, 0, InputTree.size());
-  TreeNode *expected = constructTree(OutputTree, 0, OutputTree.size());
+  bool expected = true;
 
   Solution solution;
-  TreeNode *output = solution.searchBST(input, root);
+  bool output = solution.canPlaceFlowers(flowerbed, n);
 
-  //displayAll(input);
-  //displayAll(expected);
-  //displayAll(output);
-
-  if(areIdentical(output, expected)) std::cout<<"Passed test 1"<<std::endl;
+  if(expected == output) std::cout<<"Passed test 1"<<std::endl;
   else std::cout<<"Failed test 1"<<std::endl;
 }
 
 void test2() {
-  std::vector<int> InputTree = {4,2,7,1,3};
-  std::vector<int> OutputTree = {};
+ std::vector<int> flowerbed = {1,0,0,0,1};
+  int n = 2;
 
-  int root = 5;
-
-  TreeNode *input = constructTree(InputTree, 0, InputTree.size());
-  TreeNode *expected = constructTree(OutputTree, 0, OutputTree.size());
+  bool expected = false;
 
   Solution solution;
-  TreeNode *output = solution.searchBST(input, root);
+  bool output = solution.canPlaceFlowers(flowerbed, n);
 
-  //displayAll(input);
-  //displayAll(expected);
-  //displayAll(output);
-
-  if(areIdentical(output, expected)) std::cout<<"Passed test 2"<<std::endl;
+  if(expected == output) std::cout<<"Passed test 2"<<std::endl;
   else std::cout<<"Failed test 2"<<std::endl;
 }
 

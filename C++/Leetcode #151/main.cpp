fc40290eb0132ca189c0f878d40ac5074bb4d40d
rrumana <ryan.rumana@gmail.com>
2024-05-03 10:47:43 -0700

    Added Leetcode #151

--- C++/Leetcode #151/main.cpp
+++ C++/Leetcode #151/main.cpp
@@ -1,52 +1,39 @@
-#include <iostream>         // std::cout
-#include <vector>           // std::vector
-#include <algorithm>        // std::reverse
-#include <string>           // std::string
-                            
+#include <iostream>
+#include <string>
+#include <sstream>
+
 class Solution {
-  public:
-    std::string reverseWords(std::string s) {
-      reverse(s.begin(),s.end());
-      int n=s.size();
-      int left=0;
-      int right=0;
-      int i=0;
-      while(i<n){
-        while(i<n && s[i]==' ')i++;
-        if(i==n)break; // to stop index going out of bounds
-        while(i<n && s[i]!=' '){
-          s[right++]=s[i++];
-        }
-        reverse(s.begin()+left,s.begin()+right);
-        s[right++]=' ';
-        left=right;
-        i++;
-      }
-      s.resize(right-1);
-      return s;
+public:
+  std::string reverseWords(std::string s) {
+    std::stringstream ss(s);
+    std::string word, output;
+    while(ss >> word)  {
+      output = word + " " + output;
     }
+    return output.substr(0, output.size()-1);
+  }
 };
 
 void test1() {
-  std::string input = "the sky is blue";
-  std::string expected = "blue is sky the";
+  std::string input = "The sky is blue";
+  std::string expected = "blue is sky The";
 
   Solution solution;
   std::string output = solution.reverseWords(input);
 
-  if(expected == output) std::cout<<"Passed test 1"<<std::endl;
-  else std::cout<<"Failed test 1"<<std::endl;
+  if(output == expected) std::cout<<"Passed test 1"<<std::endl;
+	else std::cout<<"Failed test 1"<<std::endl;
 }
 
 void test2() {
-  std::string input = "  hello world  ";
+  std::string input = "  hello world ";
   std::string expected = "world hello";
 
   Solution solution;
   std::string output = solution.reverseWords(input);
 
-  if(expected == output) std::cout<<"Passed test 2"<<std::endl;
-  else std::cout<<"Failed test 2"<<std::endl;
+  if(output == expected) std::cout<<"Passed test 2"<<std::endl;
+	else std::cout<<"Failed test 2"<<std::endl;
 }
 
 void test3() {
@@ -56,13 +43,12 @@ void test3() {
   Solution solution;
   std::string output = solution.reverseWords(input);
 
-  if(expected == output) std::cout<<"Passed test 3"<<std::endl;
-  else std::cout<<"Failed test 3"<<std::endl;
+  if(output == expected) std::cout<<"Passed test 3"<<std::endl;
+	else std::cout<<"Failed test 3"<<std::endl;
 }
 
 int main() {
   test1();
   test2();
   test3();
-  return 0;
 }

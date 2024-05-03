681d6452863773ecb85c345dc8fcd936be025787
rrumana <ryan.rumana@gmail.com>
2024-05-02 22:01:49 -0700

    Finished Leetcode 345

--- C++/Leetcode #334/main.cpp
+++ C++/Leetcode #334/main.cpp
@@ -9,8 +9,8 @@ public:
     int second = INT_MAX;
 
     for(int i = 0; i < nums.size(); i++) {
-      if(nums[i] < first) first = nums[i];
-      else if(nums[i] < second) second = nums[i];
+      if(nums[i] <= first) first = nums[i];
+      else if(nums[i] <= second) second = nums[i];
       else return true;
     }
 
